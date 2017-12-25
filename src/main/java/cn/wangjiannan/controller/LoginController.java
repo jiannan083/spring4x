e873fbd19a8ef839371dc5a035a308485317023e
wangjiannan <wangjiannan@nan.com>
2017-12-25 16:52:09 +0800

    c

--- src/main/java/cn/wangjiannan/controller/LoginController.java
+++ src/main/java/cn/wangjiannan/controller/LoginController.java
@@ -9,14 +9,12 @@ import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import cn.wangjiannan.common.base.BaseController;
-import cn.wangjiannan.common.shiro.captcha.DreamCaptcha;
 import cn.wangjiannan.common.util.StringUtils;
 
 /**
@@ -28,8 +26,8 @@ import cn.wangjiannan.common.util.StringUtils;
 @Controller
 public class LoginController extends BaseController {
 
-	@Autowired
-	private DreamCaptcha dreamCaptcha;
+	// @Autowired
+	// private DreamCaptcha dreamCaptcha;
 
 	@RequestMapping("/login")
 	@ResponseBody
@@ -44,9 +42,9 @@ public class LoginController extends BaseController {
 		if (StringUtils.isBlank(captcha)) {
 			throw new RuntimeException("验证码不能为空");
 		}
-		if (!dreamCaptcha.validate(request, response, captcha)) {
-			throw new RuntimeException("验证码错误");
-		}
+		// if (!dreamCaptcha.validate(request, response, captcha)) {
+		// throw new RuntimeException("验证码错误");
+		// }
 		// 1.创建Subject实例
 		Subject user = SecurityUtils.getSubject();
 		// 2.判断当前用户是否登录
---
+++ src/test/java/cn/wangjiannan/test/cache/RedisTest.java
@@ -0,0 +1,45 @@
+package cn.wangjiannan.test.cache;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.data.redis.cache.RedisCacheManager;
+
+import cn.wangjiannan.model.User;
+import cn.wangjiannan.service.UserService;
+import cn.wangjiannan.test.base.BaseTest;
+
+public class RedisTest extends BaseTest {
+	@Autowired
+	private RedisCacheManager cacheManager;
+	@Autowired
+	private UserService userService;
+
+	@Test
+	public void testRedis() {
+		Long id = 2L;
+		User user = new User();
+		user.setId(id);
+		user.setName("bb");
+
+		Cache cache = cacheManager.getCache("halfHour");
+
+		cache.put(id, user);
+
+		System.out.println("-----------" + cache.get(id, User.class));
+		Assert.assertNotNull(cache.get(id, User.class));
+	}
+
+	@Test
+	public void testRedis1() {
+		Long id = 1L;
+		// User user = new User();
+		// user.setId(id);
+		// user.setName("a");
+		// userService.save(user);
+		User user1 = userService.select(id);
+		System.out.println("--------" + user1);
+	}
+
+}
