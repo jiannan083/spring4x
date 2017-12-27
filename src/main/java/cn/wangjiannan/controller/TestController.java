795e36e9e7ecc96206f45e1c4fc9672fb2a7e2dc
wangjiannan <wangjiannan@nan.com>
2017-12-27 11:23:04 +0800

    c

--- src/main/java/cn/wangjiannan/controller/TestController.java
+++ src/main/java/cn/wangjiannan/controller/TestController.java
@@ -1,12 +1,10 @@
 package cn.wangjiannan.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import cn.wangjiannan.common.base.BaseController;
-import cn.wangjiannan.service.UserService;
 
 /**
  * 测试
@@ -17,8 +15,8 @@ import cn.wangjiannan.service.UserService;
 @Controller
 @RequestMapping("/test")
 public class TestController extends BaseController {
-	@Autowired
-	private UserService userService;
+	// @Autowired
+	// private UserService userService;
 
 	@RequestMapping("/test1")
 	@ResponseBody
