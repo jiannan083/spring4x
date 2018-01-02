ff25fcb3d1424a2d88003308a78d7324f79f840c
wangjiannan <wangjiannan@nan.com>
2018-01-02 18:00:20 +0800

    c

--- src/main/java/cn/wangjiannan/controller/TestController.java
+++ src/main/java/cn/wangjiannan/controller/TestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import cn.wangjiannan.common.base.BaseController;
-import cn.wangjiannan.model.User;
+import cn.wangjiannan.model.vo.UserVo;
 
 /**
  * 测试
@@ -48,7 +48,7 @@ public class TestController extends BaseController {
 
 	@RequestMapping("/testValidated")
 	@ResponseBody
-	public Object testValidated(@Valid User user, BindingResult bindingResult) {
+	public Object testValidated(@Valid UserVo userVo, BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
 			List<ObjectError> allErrors = bindingResult.getAllErrors();
 			for (ObjectError error : allErrors) {
